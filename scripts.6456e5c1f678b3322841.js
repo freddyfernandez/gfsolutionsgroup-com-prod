d0e0dd997b246f2903b90f2900b7beba21b8287d
freddy fernandez <fernandezyc@hotmail.com>
2022-06-07 10:57:44 -0500

    initial commit

---
+++ scripts.6456e5c1f678b3322841.js
@@ -0,0 +1,1 @@
+window.fbAsyncInit=function(){FB.init({xfbml:!0,version:"v10.0"})},function(e,t,n){var c,o=e.getElementsByTagName(t)[0];e.getElementById(n)||((c=e.createElement(t)).id=n,c.src="https://connect.facebook.net/es_ES/sdk/xfbml.customerchat.js",o.parentNode.insertBefore(c,o))}(document,"script","facebook-jssdk");
